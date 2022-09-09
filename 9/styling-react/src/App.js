f8cc79a8eafe628ba06322bc176aec83a3c9ab2f
SeongChans <fncmduddnd2@naver.com>
2022-09-09 20:18:25 +0900

    9

---
+++ 9/styling-react/src/App.js
@@ -0,0 +1,15 @@
+import SassComponent from "./SassComponent/SassComponents";
+import CSSModule from "./CSSModule/CSSModule";
+import StyledComponents from "./styled-component/StyledComponenets"
+
+function App() {
+  return (
+    <div>
+      <SassComponent />
+      <CSSModule/>
+      <StyledComponents />
+    </div>
+  );
+}
+
+export default App;
